07afaface333a07bdb51e8e8cbb74c0367a73379
Remy Chauveau <rem.chauveau@gmail.com>
2023-12-21 01:51:17 +0100

    CHORE: Update next config

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "5fhc.mypinata.cloud",
+        port: "",
+        pathname: "/ipfs/**",
+      },
+      {
+        protocol: "https",
+        hostname: "proton.mypinata.cloud",
+        port: "",
+        pathname: "/ipfs/**",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
